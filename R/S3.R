980d42c4397d4f01c7bfd83e33b3b09997276b15
Flavio Poletti <flavio.poletti@hotmail.ch>
2023-12-05 08:52:33 +0100

    export S3 methods

--- R/S3.R
+++ R/S3.R
@@ -1,9 +1,11 @@
+#' @export
 print.proporz_matrix = function(x, ...) {
     y <- as.matrix(x)
     print(y)
     invisible(x)
 }
 
+#' @export
 as.matrix.proporz_matrix = function(x, ...) {
 	matrix(x, nrow = nrow(x), dimnames = dimnames(x))
 }
