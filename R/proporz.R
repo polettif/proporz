18f6cae2994bc121d70856bde6dda497462627f4
Flavio Poletti <flavio.poletti@hotmail.ch>
2023-12-11 11:57:02 +0100

    add huntington-hill test with US data, update doc

--- R/proporz.R
+++ R/proporz.R
@@ -4,28 +4,37 @@
 #'
 #' @param votes numeric vector with number of votes for each party
 #' @param n_seats total number of seats
-#' @param method Apportionment method to use. Not case sensitive. See details.
+#' @param method Apportionment method to use, as character. Not case sensitive. See details.
 #' @param quorum Vote threshold a party must reach. Used as quota of total
 #'               votes within a district if less than 1 otherwise as number
 #'               of votes.
 #'
+#' @note Seats can also be apportioned among regions instead of parties. The parameter
+#'       \code{votes} is then normally used with census data (e.g. population counts).
+#'
 #' @details The following methods are available:
 #' \itemize{
-#'          \item{d'hondt, jefferson, hagenbach-bischoff, floor}
-#'          \item{sainte-lague, webster, round}
-#'          \item{adams, ceiling}
-#'          \item{dean, harmonic}
-#'          \item{huntington-hill, hill-huntington, geometric}
-#'          \item{hare-niemeyer, hamilton, vinton, quota_largest_remainder}
+#'          \item{d'hondt, jefferson, hagenbach-bischoff, floor (use \code{\link{divisor_floor}})}
+#'          \item{sainte-lague, webster, round (use \code{\link{divisor_round}})}
+#'          \item{adams, ceiling (use \code{\link{divisor_ceiling}})}
+#'          \item{dean, harmonic (use \code{\link{divisor_harmonic}})}
+#'          \item{huntington-hill, hill-huntington, geometric (use \code{\link{divisor_geometric}})}
+#'          \item{hare-niemeyer, hamilton, vinton, quota_largest_remainder (use \code{\link{quota_largest_remainder}})}
 #' }
 #' @examples
 #' votes = c("Party A" = 651, "Party B" = 349, "Party C" = 50)
 #'
 #' proporz(votes, 10, "sainte-lague")
+#' #> Party A Party B Party C
+#' #>       7       3       0
 #'
 #' proporz(votes, 10, "hill-huntington")
+#' #> Party A Party B Party C
+#' #>       6       3       1
 #'
 #' proporz(votes, 10, "hill-huntington", quorum = 0.05)
+#' #> Party A Party B Party C
+#' #>       6       4       0
 #'
 #'@export
 proporz = function(votes, n_seats, method, quorum = 0) {
--- tests/testthat/test-proporz.R
+++ tests/testthat/test-proporz.R
@@ -62,3 +62,26 @@ test_that("undefined result errors", {
 	             "Result is undefined: Equal remainder for two parties (position 1 & 2)",
 	             fixed = TRUE)
 })
+
+test_that("huntington-hill", {
+    # https://www.census.gov/data/tables/2020/dec/2020-apportionment-data.html
+    us_census_2020 = structure(list(
+        state = c("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska",
+                  "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"),
+        population = c(5030053, 736081, 7158923, 3013756, 39576757, 5782171, 3608298, 990837, 21570527, 10725274, 1460137, 1841377, 12822739, 6790280, 3192406, 2940865, 4509342, 4661468, 1363582, 6185278, 7033469, 10084442, 5709752, 2963914, 6160281, 1085407,
+                       1963333, 3108462, 1379089, 9294493, 2120220, 20215751, 10453948, 779702, 11808848, 3963516, 4241500, 13011844, 1098163, 5124712, 887770, 6916897, 29183290, 3275252, 643503, 8654542, 7715946, 1795045, 5897473, 577719),
+        apportioned_representatives = c(7L, 1L, 9L, 4L, 52L, 8L, 5L, 1L, 28L, 14L, 2L, 2L, 17L, 9L, 4L, 4L, 6L, 6L, 2L, 8L, 9L, 13L, 8L, 4L, 8L, 2L, 3L, 4L, 2L, 12L, 3L, 26L, 14L, 1L, 15L, 5L, 6L, 17L, 2L, 7L, 1L, 9L, 38L, 4L, 1L, 11L, 10L, 2L, 8L, 1L)),
+        row.names = c(NA, -50L), class = "data.frame")
+
+    x = proporz(us_census_2020$population, 435, "huntington-hill")
+    expect_equal(x, us_census_2020$apportioned_representatives)
+})
+
+test_that("sainte-lague", {
+    stimmen1 = c(11828277, 9990488, 6316080, 5155933, 4643272, 2830238)
+    expect_equal(divisor_round(stimmen1, 598), c(173, 146, 93, 76, 68, 42))
+
+    stimmen2 = c(430739, 143607, 242942, 1297940, 102419, 348216, 202850, 348082,
+                 2678956, 328753, 812721, 216593, 520990, 1120018, 1051198, 144464)
+    expect_equal(divisor_round(stimmen2, 146), c(6,2,4,19,2,5,3,5,39,5,12,3,8,16,15,2))
+})
